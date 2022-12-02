7aa44010d0005a721695a31e0ecef173b5d0bdfb
Archi903 <artpele@yandex.ru>
2022-12-02 14:15:03 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,16 @@ public class Main {
         }
     public static void task1 () {
 
+        int clientOS = 0;
+        int android = 1;
+        int iOS = 0;
+
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
     }
     }
