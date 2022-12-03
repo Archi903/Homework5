deeb5a9b65b8261cc77708f2ee06bcffd18bec0f
Archi903 <artpele@yandex.ru>
2022-12-03 12:44:44 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -24,23 +24,20 @@ public class Main {
     public static void task2() {
         System.out.println("Задание 2");
 
-        int clientOS = 1;
+        int clientOS = 0;
         int android = 1;
         int iOS = 0;
-        int clientDeviceYear = 2013;
+        int clientDeviceYear = 2015;
 
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        }
-        if (clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        }
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        }
-        if (clientDeviceYear < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }
-        boolean (iOS) = true;
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+        } else
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+            }
     }
 }
