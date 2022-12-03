c04f980e69941d01996b7b7154b7f26fdbbee1dd
Archi903 <artpele@yandex.ru>
2022-12-03 13:37:48 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
 
     }
 
@@ -32,12 +33,32 @@ public class Main {
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
             if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке");}
-        } else
-            if (clientOS == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-                if (clientDeviceYear < 2015) {
-                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
             }
+        } else if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задание 3");
+
+        int year = 1600;
+
+        if (year % 4 == 0) {
+            if ((year % 100 != 0) || (year % 400 == 0)) {
+                System.out.println(+year + " год является високосным");
+            }
+        }
+        if (year % 4 != 0 || year % 100 == 0) {
+            if (year % 400 != 0) {
+                System.out.println(+year + " год не является високосным");
+
+
+            }
+        }
     }
 }
